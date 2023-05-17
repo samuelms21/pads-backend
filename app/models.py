f7a9259d744da90d1cd85cf0bd6364a25a7474b2
samuelms21 <ssamuelmarcellino@gmail.com>
2023-05-17 14:57:35 +0700

    remove migrations and inject data on startup

--- app/models.py
+++ app/models.py
@@ -26,6 +26,11 @@ class Customer(db.Model):
     salesperson_id = db.Column(db.Integer, db.ForeignKey('sales_person.id'))
     orders = db.relationship('Orders', backref='customer', lazy='dynamic')
 
+    def __init__(self, _name, _address, _salesperson_id):
+        self.name = _name
+        self.address = _address
+        self.salesperson_id = _salesperson_id
+
     def __repr__(self):
         return f'<Customer {self.id}: {self.name}>'
 
@@ -39,10 +44,22 @@ class Products(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     img_src = db.Column(db.String(512))
 
+    def __init__(self, _name, _price, _warehouse_qty, _active_qty, _available_qty, _category_id, _img_src):
+        self.name = _name
+        self.price = _price
+        self.warehouse_qty = _warehouse_qty
+        self.active_qty = _active_qty
+        self.available_qty = _available_qty
+        self.category_id = _category_id
+        self.img_src = _img_src
+
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(256))
     products = db.relationship('Products', backref='category', lazy='dynamic')
+
+    def __init__(self, _category_name):
+        self.category_name = _category_name
 
 class Orders(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -50,12 +67,24 @@ class Orders(db.Model):
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
     date = db.Column(db.Date, default=date.today)
 
+    def __init__(self, _status_id, _customer_id):
+        self.status_id = _status_id
+        self.customer_id = _customer_id
+
 class OrderDetails(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
     order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
     product_qty = db.Column(db.Integer)
 
+    def __init__(self, _product_id, _order_id, _product_qty):
+        self.product_id = _product_id
+        self.order_id = _order_id
+        self.product_qty = _product_qty
+
 class Status(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     status = db.Column(db.String(100), unique=True)
+
+    def __init__(self, _status):
+        self.status = _status
--- rest_api.py
+++ rest_api.py
@@ -4,3 +4,75 @@ from app.models import SalesPerson, Customer, Products, Category, Orders, OrderD
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'SalesPerson': SalesPerson, 'Customer': Customer, 'Products': Products, 'Category': Category, 'Orders': Orders, 'OrderDetails': OrderDetails, 'Status': Status}
+
+def reset_tables():
+    with app.app_context():
+        db.drop_all()
+        db.session.commit()
+        db.create_all()
+        db.session.commit()
+
+def create_tables():
+    with app.app_context():
+        # Salesperson
+        sales_1 = SalesPerson("john_doe", "password")
+        sales_2 = SalesPerson("peter_parker", "password")
+        db.session.add(sales_1)
+        db.session.add(sales_2)
+        db.session.commit()
+        
+        # Customer
+        cust_1 = Customer("cust_1", "Jakarta", 1)
+        cust_2 = Customer("cust_2", "Bekasi", 1)
+        cust_3 = Customer("cust_3", "Depok", 1)
+        cust_4 = Customer("cust_4", "Tangerang", 2)
+        cust_5 = Customer("cust_5", "Bogor", 2)
+        cust_6 = Customer("cust_6", "Cibitung", 2)
+
+        db.session.add(cust_1)
+        db.session.add(cust_2)
+        db.session.add(cust_3)
+        db.session.add(cust_4)
+        db.session.add(cust_5)
+        db.session.add(cust_6)
+        db.session.commit()
+
+        # Category
+        tech = Category("tech")
+        fashion = Category("fashion")
+        gaming = Category("gaming")
+        db.session.add(tech)
+        db.session.add(fashion)
+        db.session.add(gaming)
+        
+        # Products
+        # Tech
+        iphone = Products("iPhone", 100, 100, 0, 100, 1, "iphone.png")
+        samsung_phone = Products("Samsung", 90, 100, 0, 100, 1, "samsung.png")
+        # Fashion
+        tshirt = Products("T-shirt", 50, 100, 0, 100, 2, "tshirt.png")
+        jeans = Products("Jeans", 40, 100, 0, 100, 2, "jeans.png")
+        # Gaming
+        razer = Products("Razer Laptop", 120, 100, 0, 100, 3, "razer_laptop.png")
+        asus_rog = Products("ASUS ROG", 110, 100, 0, 100, 3, "asus_rog.png")
+        db.session.add(iphone)
+        db.session.add(samsung_phone)
+        db.session.add(tshirt)
+        db.session.add(jeans)
+        db.session.add(razer)
+        db.session.add(asus_rog)
+        db.session.commit()
+
+        # Order Status
+        active = Status("active")
+        sent = Status("sent")
+        cancelled = Status("cancelled")
+        db.session.add(active)
+        db.session.add(sent)
+        db.session.add(cancelled)
+        db.session.commit()
+
+if __name__ == "__main__":
+    reset_tables()
+    create_tables()
+    app.run(debug=True)
